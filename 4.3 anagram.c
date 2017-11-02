66177f63043176acbec18683b014bdacd2b43def
Rishabh <33291207+rishabh2d@users.noreply.github.com>
2017-11-01 21:09:40 -0700

    Add files via upload

---
+++ 4.3 anagram.c
@@ -0,0 +1,74 @@
+// Homework 4.3 submitted by Rishabh Sharma and James Simonson
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// Function Declaration
+void anagramFinder(char firstword[], char secondword[]);
+int anagramFinderWithIO();
+// Function Definition
+void anagramFinder(char firstword[], char secondword[]) {
+    char word1[20], word2[20];
+    bool anagramChar = false;
+    int i = 0, j = 0;
+    
+    // Copying the input words into the parameters
+    strcpy(word1, firstword);
+    strcpy(word2, secondword);
+    
+    // If length is not same, print the message and terminate program
+    if(strlen(secondword) != strlen(firstword)) {
+        printf("%s is NOT an anagram of %s\n", firstword, secondword);
+        exit(0);
+    }
+    
+    for (i = 0; i< strlen(word1); i++) {
+        /* Setting the boolean to a default value of false. It should turn into true and break the loop when the word1 is exhasuted and the first for-loop does not run. */
+        anagramChar = false;
+        // Converting each letter of the word to uppercase. Can also be lowercase.
+        word1[i] = toupper(word1[i]);
+        
+        for (j = 0; j < strlen(word2); j++) {
+            // Converting each letter of the second word to uppercase.
+            word2[j] = toupper(word2[j]);
+            
+            // Checking if the letter in word 2 is present in word1.
+            //If yes, then make boolean true and convert that letter into a ' ' so that it is not read again in the loop
+            if (word1[i] == word2[j]) {
+                anagramChar = true;
+                word2[j] = ' ';
+                break;
+            }
+        }
+        if (anagramChar == false) {
+            printf("%s is NOT an anagram of %s\n", firstword, secondword);
+            exit(0);
+        }
+    }
+    if (anagramChar == true) {
+        printf("%s is an anagram of %s\n", firstword, secondword);
+    } else {
+        printf("%s is NOT an anagram of %s\n", firstword, secondword);
+    }
+}
+
+int anagramFinderWithIO(void){
+    char string1[20];
+    char string2[20];
+    
+    printf("Please enter the first word: ");
+    scanf("%s", string1);
+    printf("Please enter the second word: ");
+    scanf("%s", string2);
+    
+    anagramFinder(string1, string2);
+    return 0;
+}
+
+// Main Function
+int main(void) {
+    anagramFinderWithIO();
+}
+

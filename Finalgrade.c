0a9b86e8b561486a2bc09dd1a4444b928ab4c6d9
Rishabh <33291207+rishabh2d@users.noreply.github.com>
2017-11-21 09:39:57 -0800

    Update Finalgrade.c

--- Finalgrade.c
+++ Finalgrade.c
@@ -1,27 +1,24 @@
 /*
-This program asks for details about your current grade in class and displays the percent you need to get the final grade of your choice.
+This program asks for details about your current grade in class
+and displays the percent you need to get atleast to achieve the final grade of your choice.
 *Assuming all input is valid. 
  */
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
     char grade;
-    float percent2get, currentPercent, finalweight, finalgrade;
+    float percentToGet, currentPercent, finalweight, finalgrade;
     
     printf("Enter the grade you want in class: ");
     scanf("%s", &grade);
-    
     printf("Enter the percent you need to get that grade: ");
-    scanf("%f", &percent2get);
-    
+    scanf("%f", &percentToGet);
     printf("Enter your current percent in the class: ");
     scanf("%f", &currentPercent);
-    
     printf("Enter the weight of the final: ");
     scanf("%f", &finalweight);
     
-    finalgrade = (percent2get - ((100 - finalweight) / 100) * currentPercent) * (100 / finalweight);
+    finalgrade = (percentToGet - ((100 - finalweight) / 100) * currentPercent) * (100 / finalweight);
     
     printf("You need to get atleast: ");
     printf("%.2f", finalgrade);

d7a26136723ea9b45f2c4f44ba1f8c227a8fd3fd
Rishabh <33291207+rishabh2d@users.noreply.github.com>
2017-11-21 20:58:30 -0800

    Update 1.3 complex_mult.c

--- 1.3 complex_mult.c
+++ 1.3 complex_mult.c
@@ -1,29 +1,25 @@
+/*
+This program multiplies two complex numbers of the form ai + b.
+*/
 //CODE
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main (void)
-{
+int main(void) {
     int ai, b, ai2, b2, ai3, b3;
     
-    //Input
     printf("Enter the first complex number in the form ai + b: ");
     scanf("%d i + %d", &ai, &b);
-    
     printf("Enter the second complex number in the form ai + b: ");
     scanf("%d i + %d", &ai2, &b2);
     
-    //Multiplication
     ai3 = ai * b2 + b * ai2;
     b3 = b * b2 - ai * ai2;
-
     
-    //Output
     printf("\n");
     printf("(%di + %d)", ai, b);
     printf(" * (%di + %d)", ai2, b2);
     printf(" = %di + %d\n", ai3, b3);
-    
-
+   
     return 0;
 }

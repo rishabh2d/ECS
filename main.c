a211e5b2f0cf0de403b89fa791832dc25f5187c4
Rishabh <33291207+rishabh2d@users.noreply.github.com>
2017-11-22 20:56:27 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 /*
  This program runs a game similar to Connect Four.
- This program inputs the number of rows, number of columns
- and the number of consecutive pieces required to win the game.
+ This program inputs the number of rows, number of columns, and the number of consecutive pieces required to win the game.
  Command Line Arguement clone:  ./connectn.out 3 3 3
  */
 

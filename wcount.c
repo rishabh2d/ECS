4b04236dbd3647a7f0e275122ad96fe3a26ae668
Rishabh <33291207+rishabh2d@users.noreply.github.com>
2017-12-22 03:12:54 -0800

    Add files via upload

---
+++ wcount.c
@@ -0,0 +1,33 @@
+w#include <stdio.h>
+
+void fileWordCount(char *file_name);
+void fileWordCount(char *file_name) {
+    int counter = 0;
+    char c;
+    char SPACE = ' ';
+    char NEWLINE = '\n';
+    char DASH = '-';
+    int back2back = 0;
+    
+    FILE* file = fopen(file_name, "r");
+    
+    while ((c = getc(file)) != EOF) {
+        if (c == NEWLINE || c == SPACE) {
+            back2back++;
+            if (back2back > 1) {
+                counter--;
+            }
+            counter++;
+        } else if (c != NEWLINE || c != SPACE || c != DASH) {
+            back2back = 0;
+        }
+    }
+    
+    printf("There are %d word(s).\n", counter);
+    fclose(file);
+}
+
+int main (int argc, char *argv[]) {
+    fileWordCount(argv[1]);
+    return 0;
+}
